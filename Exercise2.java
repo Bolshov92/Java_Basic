4fc343b2e32f460e4804bc4278e5ff445a6e8d5f
Viacheslav Bolshov <137940809+Bolshov92@users.noreply.github.com>
2023-07-09 12:06:59 +0100

    Add files via upload

---
+++ Exercise2.java
@@ -0,0 +1,17 @@
+package HomeWork;
+
+public class Exercise2 {
+    public static void main(String... args) {
+        String personal = "Document";
+        String document = "certificat";
+        System.out.println(personal);
+        System.out.println(document);
+
+
+        int number = 12;
+        int number2 = 15;
+        System.out.println(number);
+        System.out.println(number2);
+
+    }
+}
---
+++ Exersice3.java
@@ -0,0 +1,23 @@
+package HomeWork;
+
+public class Exersice3 {
+
+    public static void main(String... args) {
+
+        int number1 = 30;
+       int number2 = 10;
+       int number3 = number1 + number2;
+       int number4 = number1 - number2;
+       int number5 = number1 * number2;
+       int number6 = number1 / number2;
+       System.out.println(number3);
+       System.out.println(number4);
+       System.out.println(number5);
+       System.out.println(number6);
+
+
+
+
+    }
+
+}
