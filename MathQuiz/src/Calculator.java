97e714d7fcded81aa6f8de9ba65f772bb814373c
Viacheslav Bolshov <137940809+Bolshov92@users.noreply.github.com>
2023-08-20 14:11:07 +0100

    Add files via upload

--- MathQuiz/src/Calculator.java
+++ MathQuiz/src/Calculator.java
@@ -1,25 +1,30 @@
+import java.util.Random;
+
 public class Calculator {
-        int sum ;
 
 
-    public int Add(int number1, int number2){
+
+
+
+    public int add(int number1, int number2){
         return number1 + number2;
 
 
     }
 
-    public int Subtract(int number1, int number2){
+    public int subtract(int number1, int number2){
 
         return number1 - number2;
     }
-    public int Multiply(int number1, int number2){
+    public int multiply(int number1, int number2){
 
         return number1 * number2;
 
     }
-    public int Divide(int number1, int number2){
+    public int divide(int number1, int number2){
         return number1 / number2;
 
     }
+
 }
 
--- MathQuiz/src/MathQuiz.java
+++ MathQuiz/src/MathQuiz.java
@@ -1,44 +1,57 @@
 import java.util.Random;
 import java.util.Scanner;
 
-public class MathQuiz {
-    Calculator calculator = new Calculator();
-
-    Scanner scanner = new Scanner(System.in);
-    Random random = new Random();
-
-     int score ;
 
 
 
-
-    public  void game(){
+class MathQuiz {
+    public void game() {
+        Calculator calculator = new Calculator();
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        int score = 0;
         System.out.println("Welcome to MathQuiz");
 
-        for (int i = 1; i <=6; i++ ){
+        for (int i = 0; i < 6; i++) {
+            int num1 = random.nextInt(100) + 1;
+            int num2 = random.nextInt(100) + 1;
+            int operation = random.nextInt(4);
 
-            int firstNumber = random.nextInt(100)+1;
-            int secondNumber = random.nextInt(100)+1;
-            int correctNumber =calculator.Add(firstNumber,secondNumber);
-            System.out.print("Answer for  question : "  + firstNumber + " + " + secondNumber + " :  " );
-            int userAnswer = scanner.nextInt();
-            System.out.println("Your Answer : "  + userAnswer );
-            System.out.println("Correct Answer : " + correctNumber);
-            if (userAnswer == correctNumber ){
+            String operator = "";
+            int correctAnswer = 0;
 
-                score += 5;
-                System.out.println("Your points : + 5 point");
-            }else {
-                score -=5;
-                System.out.println("You points : - 5 point");
+            switch (operation) {
+                case 0:
+                    operator = "+";
+                    correctAnswer = calculator.add(num1, num2);
+                    break;
+                case 1:
+                    operator = "-";
+                    correctAnswer = calculator.subtract(num1, num2);
+                    break;
+                case 2:
+                    operator = "*";
+                    correctAnswer = calculator.multiply(num1, num2);
+                    break;
+                case 3:
+                    operator = "/";
+                    correctAnswer = calculator.divide(num1,num2);
             }
 
+            System.out.println("Answer for question : " + num1 + " " + operator + " " + num2 );
+            int userAnswer = scanner.nextInt();
+            System.out.println("Your Answer :" + userAnswer);
+            System.out.println("Correct Answer :" + correctAnswer);
 
+            if (userAnswer == correctAnswer) {
+                System.out.println("Correct! +5 points");
+                score += 5;
+            } else {
+                System.out.println("Incorrect! -5 points");
+                score -= 5;
+            }
         }
-        System.out.println("Game Over your points : " + score );
 
+        System.out.println("Congratulations you answered all question your score : " + score);
     }
-
-
 }
-
